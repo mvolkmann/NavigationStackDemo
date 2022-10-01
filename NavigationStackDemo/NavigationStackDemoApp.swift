3c8a0631db51d04271d3cd4fbd5e38faf7456825
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-09-30 19:08:40 -0500

    Initial Commit

---
+++ NavigationStackDemo/NavigationStackDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackDemoApp.swift
+//  NavigationStackDemo
+//
+//  Created by Mark Volkmann on 9/30/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
