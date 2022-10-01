028af24f575b426b5aa7663aa12d62fe1ec0cf7f
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-10-01 09:22:03 -0500

    progress on deep links

--- NavigationStackDemo/NavigationStackDemoApp.swift
+++ NavigationStackDemo/NavigationStackDemoApp.swift
@@ -8,6 +8,25 @@ struct NavigationStackDemoApp: App {
         WindowGroup {
             ContentView()
                 .environmentObject(router)
+                .onOpenURL { url in
+                    guard let scheme = url.scheme,
+                          scheme == "NavStack" else { return }
+                    guard let country = url.host else { return }
+
+                    if let matchedCountry = Country.countries.first(where: {
+                        $0.name == country
+                    }) {
+                        router.reset()
+                        router.path.append(matchedCountry)
+                        if url.pathComponents.count == 2 {
+                            let city = url.lastPathComponent
+                            if let matchedCity = matchedCountry.cities
+                                .first(where: { $0.name == city }) {
+                                router.path.append(matchedCity)
+                            }
+                        }
+                    }
+                }
         }
     }
 }
